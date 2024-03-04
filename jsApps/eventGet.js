4e0d52eeb67440000e73be600cafb32f759b9958
Tsukioka-nekiojarashi <y-tsukioka@nekojarashi.com>
2024-03-04 18:21:34 +0900

    バグ修正

--- jsApps/eventGet.js
+++ jsApps/eventGet.js
@@ -1,3 +1,9 @@
+      /**
+       * Print the summary and start datetime/date of the next ten events in
+       * the authorized user's calendar. If no events are found an
+       * appropriate message is printed.
+       */
+
 async function listUpcomingEvents() {
     let response;
     try {
